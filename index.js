40bea7b4d8904984ef22bcb27f6db668837e95c2
siam50 <siamurrahman50@gmail.com>
2022-11-09 21:40:27 +0600

    post created for service

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
             res.send(service);
         });
 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
         // reviews api
         app.post('/reviews', async (req, res) => {
             const review = req.body;
